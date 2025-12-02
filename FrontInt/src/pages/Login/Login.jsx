b6d79b5555a9100b38dfea17d15dc6e19e6a5407
Jorge Alberto Campos Ponce <141287711+AlucardPonce@users.noreply.github.com>
2025-12-01 18:10:51 -0600

    Update Login.jsx

--- FrontInt/src/pages/Login/Login.jsx
+++ FrontInt/src/pages/Login/Login.jsx
@@ -43,7 +43,7 @@ function Login() {
             color: '#7f8c8d',
             margin: '0'
           }}>
-            Bienvenido a Descubrir Arroyo Seco aaaaaaaa
+            Bienvenido a Descubrir Arroyo Seco
           </p>
         </div>
         <FormLogin />
